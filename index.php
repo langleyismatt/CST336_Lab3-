aca74b046c5ed44a033138b6e784ba3900d99df6
mjohnson880 <morganrjohnson@gmail.com>
2016-02-22 18:40:14 +0000

    test

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Lab 3 - Morgan Johnson</title>
+        <link type="css/text" rel="stylesheet" href="css/styles.css">
+    </head>
+    <body>
+        <?php
+        ?>
+    </body>
+</html>
