70fdcb023d5e0340dbdc39ae02928f80bed00dc5
mjohnson880 <morganrjohnson@gmail.com>
2016-02-22 19:19:28 +0000

    added card array

--- index.php
+++ index.php
@@ -11,11 +11,43 @@
                 private $number;
                 private $suit;
                 
-                function __constructor($n, $s)
+                function __construct($n, $s)
                 {
                     $this->number = $n;
                     $this->suit = $s;
                 }
+                
+                function getImage()
+                {
+                    return "images/" . $this->suit . "/" . $this->number . ".png";
+                }
+                function getValue()
+                {
+                    return $this->number;
+                }
+            }
+            
+            $array = array();
+            for($i = 1; $i <= 13; ++$i)
+            {
+                $array[] = new Card($i, "clubs");
+            }
+            for($i = 13; $i >= 1; --$i)
+            {
+                $array[] = new Card($i, "diamonds");
+            }
+            for($i = 1; $i <= 13; ++$i)
+            {
+                $array[] = new Card($i, "spades");
+            }
+            for($i = 13; $i >= 1; --$i)
+            {
+                $array[] = new Card($i, "hearts");
+            }
+            
+            for($i = 0, $j = count($array); $i < $j; ++$i)
+            {
+                echo "<img src=\"" . $array[$i]->getImage() . "\" />\n";
             }
         ?>
     </body>
